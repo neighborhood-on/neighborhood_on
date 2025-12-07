c2bca9ef2c6615aa6f67b07f8d4c5cba96094824
Pandyo <keshupark0113@gmail.com>
2025-12-07 15:20:04 +0900

    comment update

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -17,16 +17,23 @@ const User = (props: any) => <svg {...props} xmlns="http://www.w3.org/2000/svg" 
 const TrendingUp = (props: any) => <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="22 7 13.5 15.5 8.5 10.5 2 17"/><polyline points="16 7 22 7 22 13"/></svg>;
 
 
+interface Comment {
+    _id: string;
+    authorName: string;
+    content: string;
+    date: string;
+}
+
 interface Post {
-  _id: string; 
-  title: string;
-  content: string;
-  authorName: string; 
-  neighborhoodId: string; 
-  category: string;
-  views: number;
-  upvotes: number;
-  date: string;
+    _id: string;
+    title: string;
+    content: string;
+    authorName: string;
+    category: string;
+    views: number;
+    upvotes: number;
+    date: string;
+    comments?: Comment[];
 }
 
 const GEOJSON_ENDPOINTS = {
